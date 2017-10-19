04e7b0595dbac9086777c7087adcd7489b254732
CaoZ <31868768+AviciiCz@users.noreply.github.com>
2017-10-19 23:05:24 +0800

    Add files via upload

---
+++ 一元二次方程实数根.cpp
@@ -0,0 +1,37 @@
+//求一元二次方程实数根，不知道对不对。。。。
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+	int a,b,c ;double delta,x1,x2;
+	cout<<"请输入abc的值："<<endl;
+	cin>>a>>b>>c;
+	do
+	{
+	cout<<"不合法的一元二次方程"<<endl;
+	} while (a=0);
+	delta=b*b-4*a*c;
+	if (delta<0)
+	{
+	cout<<"此一元二次方程没有实数根"<<endl;
+	}
+	else
+	{
+		if (delta>0)
+		{
+		double d;
+		d=sqrt(delta);
+		x1= (-b+d)/(2*a);
+		x2= (-b-d)/(a*a);
+		cout<<"x1="<<x1<<endl;
+		cout<<"x2="<<x2<<endl;
+		}
+		else
+		{
+		x1=x2= -b/(2*a);
+		cout<<"x1=x2="<<x1<<endl;
+		}
+	}
+    return 0;
+}
